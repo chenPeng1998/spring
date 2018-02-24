b97ac3e02a3b1b6da0384e6b7619d43d555465e1
chenpeng <1150660935@qq.com>
2018-02-24 15:49:25 +0800

    11

--- src/spring/condition/ConditionConfig.java
+++ src/spring/condition/ConditionConfig.java
@@ -1,6 +1,7 @@
 package spring.condition;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
  * 配置类
  */
 @Configuration
+@ComponentScan("spring.condition")
 public class ConditionConfig {
     @Bean
     @Conditional(WindowsCondition.class)
